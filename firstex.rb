4c88cc987bf3a50aa56a822bb363fe932da5e764
siva <tekisiva2012@gmail.com>
2016-02-18 18:49:07 +0530

    removed unnecessary comments

--- firstex.rb
+++ firstex.rb
@@ -1,14 +1,5 @@
 class A
 	@@arr = []
-	def add(a,b)
-		c = a+b;
-		puts c;
-	end
-
-	def sub(x,y)
-		z = x-y
-		puts z
-	end
 
 	def a(x)	
 		
@@ -27,27 +18,7 @@ end
 a = A.new
 s = [1,2,[6,9],[12,[nil,455]]]
 x = a.a(s)
-puts x
 puts x.class
 puts x.inspect
-# sindhu = A.new
-# sindhu.sub(10,5)
-
-# x,y='sindhu',45
-
-# puts "x avlue is #{x}"
-# puts "y value is #{y}"
-
-# a,b = 10,5
-# puts "#{a-b}"
-
-# puts "if else condition"
-
-# x=12
-# if x>10
-# 	puts "small #{x}"
-# else
-# 	puts "greater#{x}"
-# end
 
 
